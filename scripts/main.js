d7dbc28d70e7bfb226556dd86af0436156aa30ee
Wally Torres <wally@studiosally.com>
2018-02-06 19:20:45 +0100

    firts commit

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+	$( document ).ready(function() {
+		// $(".shareThis").simpleSocialShare();
+		$("div.shareThis").simpleSocialShare({sites: "", url: "", title: "Google", description: "Father of the Internet", image: "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png", shareType: "button", triggerButtonActiveState: false, buttonSide: "left", orientation: "horizontal"});
+	});
+
+
+
+
+	
