b2d4b96b6c75bf68af1be876b9464cbfb18cd71d
Wally Torres <wally@studiosally.com>
2018-03-27 20:52:16 +0200

    launch 27 march

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,8 @@
 	$( document ).ready(function() {
 		// $(".shareThis").simpleSocialShare();
-		$("div.shareThis").simpleSocialShare({sites: "", url: "", title: "Google", description: "Father of the Internet", image: "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png", shareType: "button", triggerButtonActiveState: false, buttonSide: "left", orientation: "horizontal"});
+		$("div.shareThis").simpleSocialShare({sites: "http://www.inteligencia.io", url: "http://www.inteligencia.io", title: "Meet INTELIGENCIA", shareType: "button", triggerButtonActiveState: false, buttonSide: "left", orientation: "horizontal"});
 	});
+
 
 
  // function onYouTubeIframeAPIReady() {
